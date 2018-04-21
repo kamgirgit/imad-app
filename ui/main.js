7a95583ad22500d1abcefd0323eb4d18fc90bc02
kamgirgit <36518334+kamgirgit@users.noreply.github.com>
2018-04-21 15:08:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,24 @@ function moveRight(){
 img.onclick = function() {
    var interval = setInterval(moveRight, 50);
 };
+
+
+// Counter code
+var button = document.getElementById('counter');
+var counter=0;
+button.onclick = function()
+{
+    
+    //make a request to the counter endpoint
+    
+    
+    //capture the response and store it in a variable
+    
+    
+    
+    //render the variable in the correct span
+    counter= counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML=counter.toString();
+    
+};
