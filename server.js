482f5812b3959e59ea8d714872374023f10cab89
kamgirgit <36518334+kamgirgit@users.noreply.github.com>
2018-04-21 23:22:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,12 +98,12 @@ app.get('/counter', function(req,res)
 });
 
 var names=[];
-app.get('/submit-name', function(req,res)  //URL: /submit-name?name=xxxx
+app.get('/submit-name/:name', function(req,res)  //URL: /submit-name?name=xxxx
 {
     
     //Get the  current name from request object
-  // var name= req.params.name;    //To Do
-    var name= req.query.name;
+   var name= req.params.name;    //To Do
+//    var name= req.query.name;
     
      names.push(name);
      
