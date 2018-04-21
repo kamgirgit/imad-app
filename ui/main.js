af520310690a58742e54d62b3e3d4002f2e918ac
kamgirgit <36518334+kamgirgit@users.noreply.github.com>
2018-04-21 16:30:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,20 +19,36 @@ img.onclick = function() {
 
 // Counter code
 var button = document.getElementById('counter');
-var counter=0;
+//var counter=0;
 button.onclick = function()
 {
     
-    //make a request to the counter endpoint
-    
+    // create a request object
+    var request= new XMLHttpRequest();
     
     //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState== XMLHttpRequest.DONE){
+
+            //take some action
+            if(request.status==200){
+                var counter= request.responseText;
+                 var span = document.getElementById('count');
+                  span.innerHTML=counter.toString();
+
+            }
+        }
+        
+        // not done yet
+    };
     
     
+    // make a request 
+    request.open('GET', 'http://');
     
     //render the variable in the correct span
-    counter= counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML=counter.toString();
+   // counter= counter + 1;
+    //var span = document.getElementById('count');
+    //span.innerHTML=counter.toString();
     
 };
