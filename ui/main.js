763b59417fcabb40ddf21f3f64cd11c14c93085c
kamgirgit <36518334+kamgirgit@users.noreply.github.com>
2018-04-22 00:05:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -55,8 +55,7 @@ button.onclick = function()
 };
 
 //Submit names
-var nameInput= document.getElementById('name');
-var name= nameInput.value;
+
 var submit= document.getElementById('submit_btn');
 submit.onclick= function() 
 {
@@ -87,6 +86,8 @@ submit.onclick= function()
     
     
     // make a request 
+    var nameInput= document.getElementById('name');
+    var name= nameInput.value;
     request.open('GET', 'http://kamranjmi12.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
   
