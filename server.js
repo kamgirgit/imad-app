594961f38ea2e396d41c45366e0f55de9f3fb3d7
kamgirgit <36518334+kamgirgit@users.noreply.github.com>
2018-04-24 12:25:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,6 +99,14 @@ function createTemplate (data) {
         return htmlTemplate;
 }
  
+ app.get('/test-db', function(req,res)
+ {
+    //make a select request 
+    
+    // return a response with the results 
+ });
+ 
+ 
  
  app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
