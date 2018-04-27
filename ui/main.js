c1ab6ce42b8f0458db935e794815bf73e45eb71b
kamgirgit <36518334+kamgirgit@users.noreply.github.com>
2018-04-28 01:46:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -90,11 +90,46 @@ submit.onclick= function()
     var name= nameInput.value;
     request.open('GET', 'http://kamranjmi12.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
-  
-  
-   
-  
-  
-  
-  
 };
+
+
+//Submit username/password to login
+
+var submit= document.getElementById('submit_btn');
+submit.onclick= function() 
+{
+ var request= new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState== XMLHttpRequest.DONE){
+
+            //take some action
+            if(request.status==200){
+               //capture a list of names and render it as a list
+                  console.log('user logged in');
+                  alert('Logged in Successfully');
+            } else if(request.status === 403) {
+                alert('username/password is incorrect');
+            } else if (request.status === 500) {
+                alert('Something went wrong on the server');
+            }
+        }
+        
+        // not done yet
+    };
+    
+    
+    // make a request 
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://kamranjmi12.imad.hasura-app.io/login ', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+};
+
+
+
+
