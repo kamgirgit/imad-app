3ca53071046446f7c14652229297c990febe6f94
kamgirgit <36518334+kamgirgit@users.noreply.github.com>
2018-04-27 16:03:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,6 +88,23 @@ function createTemplate (data) {
     var hashedString = hash(req.params.input,'this-is-some random string');
     res.send(hashedString);
  });
+ 
+ app.post('/create-user', function(req,res)
+ {  //username, password
+ 
+   // JSON
+   var username=req.body.username;
+   var password=req.body.password;
+   var salt= crypto.getRandomBytes(128).toString('hex');
+   var dbString= hash(password, salt);  
+   pool.query('INSERT INTO "user" (username,password) VALUES ($1, $2)', [username, dbString], function(err, result) {
+        if(err) {
+           res.status(500).send(err.toString());
+       } else {
+           res.send('user successfully created:' + username);
+       }
+   });
+ });
 
 
  var pool= new Pool(config);
